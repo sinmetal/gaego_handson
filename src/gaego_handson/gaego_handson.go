f95e6d83fce439e20eaaf9e09e98f49e1c81c289
Shingo Ishimura <metal.tie@gmail.com>
2016-07-16 10:41:29 +0900

    ログ出力を追加

--- src/gaego_handson/gaego_handson.go
+++ src/gaego_handson/gaego_handson.go
@@ -3,6 +3,9 @@ package gaego_handson
 import (
 	"fmt"
 	"net/http"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
 )
 
 func init() {
@@ -10,5 +13,8 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+
+	log.Infof(c, "Hello %s", "world")
 	fmt.Fprint(w, "Hello, world!")
 }
